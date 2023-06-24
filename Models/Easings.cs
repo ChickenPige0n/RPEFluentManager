73cea2a817143974356a54bb78578e00ddd6a7bc
ChickenPige0n <2336983354@qq.com>
2023-06-24 14:06:02 +0800

    导入之前写的一些轮子

---
+++ Models/Easings.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventFitter
+{
+    internal class Easings
+    {
+        //constants for Back
+        const double c1 = 1.70158;
+        const double c2 = c1 * 1.525;
+        const double c3 = c1 + 1;
+        const double c4 = (2 * Math.PI) / 3;
+        const double n1 = 7.5625;
+        const double d1 = 2.75;
+        const double c5 = (2 * Math.PI) / 4.5;
+
+
+        //1
+        public static double EaseLinear(double x)
+        {
+            return x;
+        }
+        //2
+        public static double EaseOutSin(double x)
+        {
+            return Math.Sin((x * Math.PI) / 2);
+        }
+        //3
+        public static double EaseInSin(double x)
+        {
+            return 1 - Math.Cos((x * Math.PI) / 2);
+        }
+        //4
+        public static double EaseOutQuad(double x)
+        {
+            return 1 - (1 - x) * (1 - x);
+        }
+        //5
+        public static double EaseInQuad(double x)
+        {
+            return x * x;
+        }
+        //6
+        public static double EaseInOutSin(double x)
+        {
+            return -(Math.Cos(Math.PI * x) - 1) / 2;
+        }
+        //7
+        public static double EaseInOutQuad(double x)
+        {
+            return x < 0.5 ? 2 * x * x : 1 - Math.Pow(-2 * x + 2, 2) / 2;
+        }
+        //8
+        public static double EaseOutCubic(double x)
+        {
+            return 1 - Math.Pow(1 - x, 3);
+        }
+        //9
+        public static double EaseInCubic(double x)
+        {
+            return x * x * x;
+        }
+        //10
+        public static double EaseOutQuart(double x)
+        {
+            return 1 - Math.Pow(1 - x, 4);
+        }
+        //11
+        public static double EaseInQuart(double x)
+        {
+            return x * x * x * x;
+        }
+        //12
+        public static double EaseInOutCubic(double x)
+        {
+            return x < 0.5 ? 4 * x * x * x : 1 - Math.Pow(-2 * x + 2, 3) / 2;
+        }
+        //13
+        public static double EaseInOutQuart(double x)
+        {
+            return x < 0.5 ? 8 * x * x * x * x : 1 - Math.Pow(-2 * x + 2, 4) / 2;
+        }
+        //14
+        public static double EaseOutQuint(double x)
+        {
+            return 1 - Math.Pow(1 - x, 5);
+        }
+        //15
+        public static double EaseInQuint(double x)
+        {
+            return x * x * x * x * x;
+        }
+        //16
+        public static double EaseOutExpo(double x)
+        {
+            return x == 1 ? 1 : 1 - Math.Pow(2, -10.0 * x);
+        }
+        //17
+        public static double EaseInExpo(double x)
+        {
+            return x == 0 ? 0 : Math.Pow(2, 10 * x - 10);
+        }
+        //18
+        public static double EaseOutCirc(double x)
+        {
+            return Math.Sqrt(1 - Math.Pow(x - 1, 2));
+        }
+        //19
+        public static double EaseInCirc(double x)
+        {
+            return 1 - Math.Sqrt(1 - Math.Pow(x, 2));
+        }
+        //20
+        public static double EaseOutBack(double x)
+        {
+            return 1 + c3 * Math.Pow(x - 1, 3) + c1 * Math.Pow(x - 1, 2);
+        }
+        //21
+        public static double EaseInBack(double x)
+        {
+            return c3 * x * x * x - c1 * x * x;
+        }
+        //22
+        public static double EaseInOutCirc(double x)
+        {
+            return x < 0.5
+            ? (1 - Math.Sqrt(1 - Math.Pow(2 * x, 2))) / 2
+            : (Math.Sqrt(1 - Math.Pow(-2 * x + 2, 2)) + 1) / 2;
+        }
+        //23
+        public static double EaseInOutBack(double x)
+        {
+            return x < 0.5
+            ? (Math.Pow(2 * x, 2) * ((c2 + 1) * 2 * x - c2)) / 2
+            : (Math.Pow(2 * x - 2, 2) * ((c2 + 1) * (x * 2 - 2) + c2) + 2) / 2;
+        }
+        //24
+        public static double EaseOutElastic(double x)
+        {
+            return x == 0
+              ? 0
+              : x == 1
+              ? 1
+              : Math.Pow(2, -10 * x) * Math.Sin((x * 10 - 0.75) * c4) + 1;
+        }
+        //25
+        public static double EaseInElastic(double x)
+        {
+            return x == 0
+              ? 0
+              : x == 1
+              ? 1
+              : -Math.Pow(2, 10 * x - 10) * Math.Sin((x * 10 - 10.75) * c4);
+        }
+        //26
+        public static double EaseOutBounce(double x)
+        {
+            if (x < 1 / d1)
+            {
+                return n1 * x * x;
+            }
+            else if (x < 2 / d1)
+            {
+                return n1 * (x -= 1.5 / d1) * x + 0.75;
+            }
+            else if (x < 2.5 / d1)
+            {
+                return n1 * (x -= 2.25 / d1) * x + 0.9375;
+            }
+            else
+            {
+                return n1 * (x -= 2.625 / d1) * x + 0.984375;
+            }
+            
+        }
+        //27
+        public static double EaseInBounce(double x)
+        {
+            return 1 - EaseOutBounce(1 - x);
+        }
+        //28
+        public static double EaseInOutBounce(double x)
+        {
+            return x< 0.5
+              ? (1 - EaseOutBounce(1 - 2 * x)) / 2
+              : (1 + EaseOutBounce(2 * x - 1)) / 2;
+        }
+
+
+        public delegate double EasingFunc(double x);
+
+        public static EasingFunc[] easeFuncs = {
+            new EasingFunc(EaseLinear),
+            new EasingFunc(EaseOutSin),
+            new EasingFunc(EaseInSin),
+            new EasingFunc(EaseOutQuad),
+            new EasingFunc(EaseInQuad),//5
+            new EasingFunc(EaseInOutSin),
+            new EasingFunc(EaseInOutQuad),
+            new EasingFunc(EaseOutCubic),
+            new EasingFunc(EaseInCubic),
+            new EasingFunc(EaseOutQuart),//10
+            new EasingFunc(EaseInQuart),
+            new EasingFunc(EaseInOutCubic),
+            new EasingFunc(EaseInOutQuart),
+            new EasingFunc(EaseOutQuint),
+            new EasingFunc(EaseInQuint),//15
+            new EasingFunc(EaseOutExpo),
+            new EasingFunc(EaseInExpo),
+            new EasingFunc(EaseOutCirc),
+            new EasingFunc(EaseInCirc),
+            new EasingFunc(EaseOutBack),//20
+            new EasingFunc(EaseInBack),
+            new EasingFunc(EaseInOutCirc),
+            new EasingFunc(EaseInOutBack),
+            new EasingFunc(EaseOutElastic),
+            new EasingFunc(EaseInElastic),//25
+            new EasingFunc(EaseOutBounce),
+            new EasingFunc(EaseInBounce),
+            new EasingFunc(EaseInOutBounce)
+        };
+    }
+}
---
+++ Models/RPEChart.cs
@@ -0,0 +1,306 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+//generated automatically
+
+namespace EventFitter
+{
+
+    public class BPMListItem
+    {
+        public float bpm { get; set; }
+        public Time startTime { get; set; }
+    }
+    public class META
+    {
+        public int RPEVersion { get; set; }
+        public string background { get; set; }
+        public string charter { get; set; }
+        public string composer { get; set; }
+        public string id { get; set; }
+        public string level { get; set; }
+        public string name { get; set; }
+        public int offset { get; set; }
+        public string song { get; set; }
+    }
+    public class AlphaControlItem
+    {
+        public AlphaControlItem(float xx)
+        {
+            alpha = 1.0f;
+            easing = 1;
+            x = xx;
+        }
+        public float alpha { get; set; }
+        public int easing { get; set; }
+        public float x { get; set; }
+    }
+    public class RPEEvent
+    {
+        public RPEEvent()
+        {
+            linkgroup = 0;
+            easingLeft = 0;
+            easingRight = 1;
+        }
+
+
+        public int GetMontonicity()
+        {
+            if(end > start)
+            {
+                return 1;
+            }
+            else if(end < start)
+            {
+                return -1;
+            }
+            return 0;
+        }
+        public double getDuration()
+        {
+            return endTime.getTime() - startTime.getTime();
+        }
+        public double getVelocity()
+        {
+            return (end-start)/getDuration();
+        }
+
+        public float easingLeft { get; set; }
+        public float easingRight { get; set; }
+        public int easingType { get; set; }
+        public float end { get; set; }
+        public Time endTime { get; set; }
+        public int linkgroup { get; set; }
+        public float start { get; set; }
+        public Time startTime { get; set; }
+    }
+
+    public class Time : List<int>
+    {
+        public double getTime()
+        {
+            return this[2] == 0 ? 0 : (double)this[0] + ((double)this[1] / (double)this[2]);
+        }
+    }
+
+
+    public class SpeedEventsItem
+    {
+        public float end { get; set; }
+        public Time endTime { get; set; }
+        public int linkgroup { get; set; }
+        public float start { get; set; }
+        public Time startTime { get; set; }
+    }
+
+
+
+    public class EventLayersItem
+    {
+        [JsonIgnore]
+        private List<RPEEvent> _alphaEvents;
+        [JsonIgnore]
+        private List<RPEEvent> _moveXEvents;
+        [JsonIgnore]
+        private List<RPEEvent> _moveYEvents;
+        [JsonIgnore]
+        private List<RPEEvent> _rotateEvents;
+        [JsonIgnore]
+        private List<SpeedEventsItem> _speedEvents;
+
+        public List<RPEEvent> alphaEvents
+        {
+            get { return _alphaEvents; }
+            set { _alphaEvents = value; }
+        }
+
+        public List<RPEEvent> moveXEvents
+        {
+            get { return _moveXEvents; }
+            set { _moveXEvents = value; }
+        }
+
+        public List<RPEEvent> moveYEvents
+        {
+            get { return _moveYEvents; }
+            set { _moveYEvents = value; }
+        }
+
+        public List<RPEEvent> rotateEvents
+        {
+            get { return _rotateEvents; }
+            set { _rotateEvents = value; }
+        }
+
+        public List<SpeedEventsItem> speedEvents
+        {
+            get { return _speedEvents; }
+            set { _speedEvents = value; }
+        }
+
+        private ref List<RPEEvent> GetEventsByType(string type)
+        {
+            switch (type)
+            {
+                case "alpha":
+                    return ref _alphaEvents;
+
+                case "moveX":
+                    return ref _moveXEvents;
+
+                case "moveY":
+                    return ref _moveYEvents;
+
+                case "rotate":
+                    return ref _rotateEvents;
+
+                default:
+                    return ref _moveXEvents;
+            }
+        }
+
+        private void GetEventInRange(string EventType, int baseIndex, int eventCount, out double[] values, out double[] beatTimes, out double beatTimeRange)
+        {
+            List<RPEEvent> Events = GetEventsByType(EventType);
+
+            values = new double[eventCount + 1];
+            beatTimes = new double[eventCount];
+
+            for (int outIndex = baseIndex; outIndex < baseIndex + eventCount; outIndex++)
+            {
+                // 获取当前事件的start值和前一个事件到当前事件的duration
+                values[outIndex - baseIndex] = Events[outIndex].start;
+                beatTimes[outIndex - baseIndex] = Events[outIndex].getDuration();
+            }
+
+            // 最后一个事件的end值存储到values数组的最后一个位置
+            values[^1] = Events[baseIndex + eventCount - 1].end;
+
+            // 计算beatTimeRange
+            beatTimeRange = Events[baseIndex + eventCount - 1].endTime.getTime() - Events[baseIndex].startTime.getTime();
+        }
+
+        //<summary>
+        //1:moveX 2:moveY 3:rotation 4:alpha 5:speed
+        //</summary>
+        private void ReplaceEvent(RPEEvent eventToAdd, int baseIndex, int removeCount, string EventType)
+        {
+            List<RPEEvent> eventsToBeReplaced = GetEventsByType(EventType);
+            eventsToBeReplaced.RemoveRange(baseIndex, removeCount);
+            eventsToBeReplaced.Insert(baseIndex, eventToAdd);
+        }
+    }
+
+
+    public class Extended
+    {
+        public Extended() {
+            RPEEvent defaultInc = new RPEEvent();
+            defaultInc.start = 0.0f;
+            defaultInc.end = 0.0f;
+            defaultInc.easingType = 1;
+            defaultInc.startTime = new Time { 0, 0, 1 };
+            defaultInc.endTime = new Time { 1, 0, 1 };
+            inclineEvents = new List<RPEEvent>
+            {
+                defaultInc
+            };
+        }
+        public List<RPEEvent> inclineEvents { get; set; }
+    }
+
+    public class NotesItem
+    {
+        public int above { get; set; }
+        public int alpha { get; set; }
+        public Time endTime { get; set; }
+        public int isFake { get; set; }
+        public float positionX { get; set; }
+        public float size { get; set; }
+        public float speed { get; set; }
+        public Time startTime { get; set; }
+        public int type { get; set; }
+        public float visibleTime { get; set; }
+        public float yOffset { get; set; }
+    }
+
+    public class PosControlItem
+    {
+        public PosControlItem(float xx)
+        {
+            pos = 1.0f;
+            easing = 1;
+            x = xx;
+        }
+        public int easing { get; set; }
+        public float pos { get; set; }
+        public float x { get; set; }
+    }
+    public class SizeControlItem
+    {
+        public SizeControlItem(float xx)
+        {
+            size = 1.0f;
+            easing = 1;
+            x = xx;
+        }
+        public int easing { get; set; }
+        public float size { get; set; }
+        public float x { get; set; }
+    }
+    public class SkewControlItem
+    {
+        public SkewControlItem(float xx)
+        {
+            skew = 0.0f;
+            easing = 1;
+            x = xx;
+        }
+        public int easing { get; set; }
+        public float skew { get; set; }
+        public float x { get; set; }
+    }
+    public class YControlItem
+    {
+        public YControlItem(float xx)
+        {
+            y = 1.0f;
+            easing = 1;
+            x = xx;
+        }
+        public int easing { get; set; }
+        public float x { get; set; }
+        public float y { get; set; }
+    }
+    public class JudgeLineListItem
+    {
+        public int @Group { get; set; }
+        public string Name { get; set; }
+        public string Texture { get; set; }
+        public List<AlphaControlItem> alphaControl { get; set; }
+        public float bpmfactor { get; set; }
+        public List<EventLayersItem> eventLayers { get; set; }
+        public Extended extended { get; set; }
+        public int father { get; set; }
+        public int isCover { get; set; }
+        public List<NotesItem> notes { get; set; }
+        public int numOfNotes { get; set; }
+        public List<PosControlItem> posControl { get; set; }
+        public List<SizeControlItem> sizeControl { get; set; }
+        public List<SkewControlItem> skewControl { get; set; }
+        public List<YControlItem> yControl { get; set; }
+        public int zOrder { get; set; }
+    }
+    public class RPEChart
+    {
+        public List<BPMListItem> BPMList { get; set; }
+        public META META { get; set; }
+        public List<string> judgeLineGroup { get; set; }
+        public List<JudgeLineListItem> judgeLineList { get; set; }
+    }
+}
