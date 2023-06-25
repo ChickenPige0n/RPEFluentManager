5846f1b1e1c7d14f57d51ad6822f16f3216a4ef5
ChickenPige0n <2336983354@qq.com>
2023-06-25 18:48:14 +0800

    再次修改路径错误处理

--- Models/RPEManagerSettings.cs
+++ Models/RPEManagerSettings.cs
@@ -25,22 +25,38 @@ namespace RPEFluentManager.Models
 
         public static RPEManagerSettings GetSettings()
         {
+
             if (!File.Exists(settingsPath))
             {
-
-                OpenFileDialog openFileDialog = new OpenFileDialog() { FileName = "PhiEdit.exe", Title = "第一次打开时请先选择要管理的RPE可执行文件" };
-                if (openFileDialog.ShowDialog() == true)
-                {
-                    var initialSettings = new RPEManagerSettings() { ResourcePath = Path.Combine(Path.GetDirectoryName(openFileDialog.FileName), "Resources") };
-                    string json = JsonSerializer.Serialize(initialSettings);
-                    File.WriteAllText(settingsPath, json);
-                }
-                else
-                {
-                    Application.Current.Shutdown();
-                }
+                OnFirstOpen("第一次打开时请先选择要管理的RPE可执行文件");
             }
-            return JsonSerializer.Deserialize<RPEManagerSettings>(File.ReadAllText(settingsPath));
+
+            RPEManagerSettings settings = JsonSerializer.Deserialize<RPEManagerSettings>(File.ReadAllText(settingsPath));
+
+            if (!Directory.Exists(settings.ResourcePath))
+            {
+                OnFirstOpen("Resources文件不正确，请重新选择RPE可执行文件");
+                settings = JsonSerializer.Deserialize<RPEManagerSettings>(File.ReadAllText(settingsPath));
+            }
+
+
+
+            return settings;
+        }
+
+        private static void OnFirstOpen(string annotation)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog() { FileName = "PhiEdit.exe", Title = annotation };
+            if (openFileDialog.ShowDialog() == true)
+            {
+                var initialSettings = new RPEManagerSettings() { ResourcePath = Path.Combine(Path.GetDirectoryName(openFileDialog.FileName), "Resources") };
+                string json = JsonSerializer.Serialize(initialSettings);
+                File.WriteAllText(settingsPath, json);
+            }
+            else
+            {
+                Application.Current.Shutdown();
+            }
         }
 
         public static void SaveSettings(RPEManagerSettings settings)
